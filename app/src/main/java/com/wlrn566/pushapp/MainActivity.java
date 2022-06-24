0f5c3a636a85be206e94fda556e67251b1cb2ef4
wlrn566 <wlrn567@gmail.com>
2022-06-24 14:02:53 +0900

    220624 현재 위치 좌표 띄우기 기능

--- app/src/main/java/com/wlrn566/pushapp/MainActivity.java
+++ app/src/main/java/com/wlrn566/pushapp/MainActivity.java
@@ -1,81 +1,261 @@
 package com.wlrn566.pushapp;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapReverseGeoCoder;
 import net.daum.mf.map.api.MapView;
 
+import org.w3c.dom.Text;
+
+import java.io.IOException;
 import java.security.MessageDigest;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = getClass().getName();
+    private GpsTracker gpsTracker;
 
-    private Permissions permission;
-    boolean ck;
+    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+    String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION
+            , Manifest.permission.ACCESS_COARSE_LOCATION};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
-        MapView mapView = new MapView(this);
-
-        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
 
         // 권한 체크
-        permissionCheck();
-        Log.d(TAG, "permission ck = " + ck);
+        if (!checkLocationServiceStatus()) {  // GPS
+            Log.d(TAG, "check Location Permission");
+            showDialogForLocationServiceSetting();
+        } else {
+            checkRunTimePermission();
+        }
 
-        // 지도 나타내기
-        setMap(mapView, mapViewContainer);
+        TextView add_tv = (TextView) findViewById(R.id.add_tv);
+        TextView lat_tv = (TextView) findViewById(R.id.lat_tv);
+        TextView lng_tv = (TextView) findViewById(R.id.lng_tv);
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-//        if (ck) {
-//        setMap(mapView, mapViewContainer);  // 권한이 있다면 지도 나타내기
-//        } else {
-//            exitProgram();  // 권한 없으면 꺼버리기
-//        }
+                gpsTracker = new GpsTracker(MainActivity.this);
 
-//        MainFragment mainFragment = new MainFragment();
-//        getSupportFragmentManager().beginTransaction().replace(R.id.main, mainFragment).commit();
-//        permissionCheck();
+                double latitude = gpsTracker.getLatitude();
+                double longitude = gpsTracker.getLongitude();
+
+                String address = getCurrentAddress(latitude, longitude);
+                add_tv.setText(address);
+                lat_tv.setText(String.valueOf(latitude));
+                lng_tv.setText(String.valueOf(longitude));
+
+                Toast.makeText(MainActivity.this, "현재위치 \n위도 " + latitude + "\n경도 " + longitude, Toast.LENGTH_LONG).show();
+
+            }
+        });
 
         getAppKeyHash();
     }
 
-    private void setMap(MapView mapView, ViewGroup mapViewContainer) {
-
-        mapViewContainer.addView(mapView);
-
-        // 중심점 변경
-        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true);
-
-        // 줌 레벨 변경
-        mapView.setZoomLevel(7, true);
-
-        // 중심점 변경 + 줌 레벨 변경
-        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(33.41, 126.52), 9, true);
-
-        // 줌 인
-        mapView.zoomIn(true);
-
-        // 줌 아웃
-        mapView.zoomOut(true);
+    void checkRunTimePermission() {
+        // 위치 권한 있는지 확인하기
+        int hasFineLocationPermission = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+        int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED && hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {
+            // 가지고 있다면 위치값 가져올 수 있음
+            Log.d(TAG, "PERMISSION_GRANTED");
+        } else {  // 권한을 요청한적이 있는데 거부한적이 있음
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, REQUIRED_PERMISSIONS[0])) {  // 거부한 적 있을 때
+                Toast.makeText(MainActivity.this, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG).show();  // 요청 이유
+                ActivityCompat.requestPermissions(MainActivity.this, REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE);  // 요청
+            } else {  // 처음 요청 시
+                ActivityCompat.requestPermissions(MainActivity.this, REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE);  // 요청
+            }
+        }
     }
+
+    // checkRunTimePermission의 request 결과값
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // 요청코드가 맞고 요청개수만큼 request 되었을 때
+        if (requestCode == PERMISSIONS_REQUEST_CODE && grantResults.length == REQUIRED_PERMISSIONS.length) {
+            boolean check_result = true;
+            // 모든 권한을 허용했는지 확인
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false;
+                    break;
+                }
+            }
+            if (check_result) {
+                // 위치 값 가져오기 가능
+                Log.d(TAG, "permission success");
+            } else {
+                // 거부된 권한이 있을 때
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0]) || ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[1])) {
+                    Toast.makeText(MainActivity.this, "권한이 거부 되었습니다. 다시 실행해주세요", Toast.LENGTH_LONG).show();
+                    finish();
+                } else {
+                    Toast.makeText(MainActivity.this, "권한이 거부 되었습니다. 다시 실행해주세요", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+    public String getCurrentAddress(double latitude, double longitude) {
+
+        //지오코더 - GPS를 주소로 변환
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+        List<Address> addresses;
+
+        try {
+
+            addresses = geocoder.getFromLocation(
+                    latitude,
+                    longitude,
+                    7);
+        } catch (IOException ioException) {
+            //네트워크 문제
+            Toast.makeText(this, "Geocoder failed", Toast.LENGTH_LONG).show();
+            return "지오코더 서비스 사용불가";
+        } catch (IllegalArgumentException illegalArgumentException) {
+            Toast.makeText(this, "wrong GPS coordinate", Toast.LENGTH_LONG).show();
+            return "잘못된 GPS 좌표";
+
+        }
+
+        if (addresses == null || addresses.size() == 0) {
+            Toast.makeText(this, "address undetected", Toast.LENGTH_LONG).show();
+            return "주소 미발견";
+
+        }
+
+        Address address = addresses.get(0);
+        return address.getAddressLine(0).toString() + "\n";
+
+    }
+
+//    // 현재 위치 업데이트
+//    @Override
+//    public void onCurrentLocationUpdate(MapView mapView, MapPoint mapPoint, float v) {
+//        MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();  // 좌표 가져오기
+//        Log.i(TAG, String.format("MapView onCurrentLocationUpdate (%f,%f) accuracy (%f)", mapPointGeo.latitude, mapPointGeo.longitude, v));
+//        currentMapPoint = MapPoint.mapPointWithGeoCoord(mapPointGeo.latitude, mapPointGeo.longitude);  // 지도 중심에 넣을 현재 좌표
+//        mapView.setMapCenterPoint(currentMapPoint, true);
+//        // 현재 좌표 저장
+//        mCurrentLat = mapPointGeo.latitude;
+//        mCurrentLng = mapPointGeo.longitude;
+//        Log.d(TAG, "current lat = " + mCurrentLat + " current lng = " + mCurrentLng);
+//    }
+
+//    @Override
+//    public void onCurrentLocationDeviceHeadingUpdate(MapView mapView, float v) {
+//
+//    }
+//
+//    @Override
+//    public void onCurrentLocationUpdateFailed(MapView mapView) {
+//
+//    }
+//
+//    @Override
+//    public void onCurrentLocationUpdateCancelled(MapView mapView) {
+//
+//    }
+//
+//    @Override
+//    public void onReverseGeoCoderFoundAddress(MapReverseGeoCoder mapReverseGeoCoder, String s) {
+//
+//    }
+//
+//    @Override
+//    public void onReverseGeoCoderFailedToFindAddress(MapReverseGeoCoder mapReverseGeoCoder) {
+//
+//    }
+
+    private void showDialogForLocationServiceSetting() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("위치 서비스 비활성화");
+        builder.setMessage("앱을 사용하기 위해 위치 서비스가 필요합니다.");
+        builder.setCancelable(true);
+        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent callGPSSettingIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
+            }
+        });
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.create().show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case GPS_ENABLE_REQUEST_CODE:
+                // GPS 활성화 여부 검사
+                if (checkLocationServiceStatus()) {
+                    if (checkLocationServiceStatus()) {
+                        Log.d("Active", "onActivityResult : GPS ON");
+                        checkRunTimePermission();
+                        return;
+                    }
+                }
+                break;
+        }
+
+    }
+
+    public boolean checkLocationServiceStatus() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
 
     @Override
     protected void onStart() {
@@ -84,68 +264,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    // 키해시 값
-    private void getAppKeyHash() {
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md;
-                md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                String something = new String(Base64.encode(md.digest(), 0));
-                Log.d(TAG, "Hash key = " + something);
-            }
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            Log.e(TAG, "name not found" + e.toString());
-        }
-    }
-
-    private void permissionCheck() {
-        permission = new Permissions(this, this);
-        // 권한 체크
-        // 권한이 없다면 (false) 요청하기
-        if (permission.check() == false) {
-            permission.request();
-        } else {
-            ck = true;
-        }
-    }
-
-    // permissions의 request 결과값
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        // 요청이 거부
-        if (permission.result(requestCode, permissions, grantResults) == false) {
-            ck = false;
-            // 다시 요청
-            permission.request();
-        } else {
-            ck = true;
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-    private void exitProgram() {
-        // 태스크를 백그라운드로 이동
-        moveTaskToBack(true);
-
-        if (Build.VERSION.SDK_INT >= 21) {
-            // 액티비티 종료 + 태스크 리스트에서 지우기
-            finishAndRemoveTask();
-        } else {
-            // 액티비티 종료
-            finish();
-        }
-        System.exit(0);
-    }
-
     @Override
     protected void onResume() {
         super.onResume();
         Log.d(TAG, "onResume");
-
-
     }
 
     @Override
@@ -165,4 +287,22 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         Log.d(TAG, "onDestroy");
     }
+
+    // 키해시 값
+    private void getAppKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                Log.d(TAG, "Hash key = " + something);
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            Log.e(TAG, "name not found" + e.toString());
+        }
+    }
+
 }
